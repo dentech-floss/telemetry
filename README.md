78e34b312b909ad0edd62edab492f7ce0a5c0767
Andreas Lindfalk <andreas.lindfalk@dentech.se>
2022-05-17 16:51:46 +0200

    creating the lib

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
 # telemetry
+
+Setup [Opentelemetry](https://github.com/open-telemetry/opentelemetry-go) support, currently only tracing but metrics and logs can/will be added when needed/supported. 
+
+### Tracing
+
+The spans can be exported to either an [Opentelemetry Collector](https://github.com/open-telemetry/opentelemetry-collector) or just to stdout for local development usage. Both of these can't be enabled at the same time, first we check if the "OtlpExporterEnabled" flag is true and if that is not the case then we check if the "StdoutExporterEnabled" flag is true.
+
+## Install
+
+```
+go get github.com/dentech-floss/telemetry@v0.1.0
+```
+
+## Usage
+
+### Tracing
+
+```go
+package example
+
+import (
+    "context"
+    "github.com/dentech-floss/telemetry/pkg/telemetry"
+)
+
+func main() {
+    ctx := context.Background()
+    shutdownTracing := telemetry.SetupTracing(
+        ctx,
+        &telemetry.TracingConfig{
+            ServiceName: "app name",
+            ServiceVersion: "github tag",
+            DeploymentEnvironment: "gcp project id",
+            OtlpExporterEnabled: true, // whether or not to export traces to a collector
+            // OtlpCollectorHttpEndpoint: ..., // defaults to "opentelemetry-collector:80" if not set
+            // OtlpCollectorTimeoutSecs: ...,  // default to 30 if not set
+            // StdoutExporterEnabled: ...,     // if OtlpExporterEnabled is false, then you can enable this for stdout exporting
+        },
+    )
+    defer shutdownTracing()
+}
+```
+
+Now you can instrument different parts of your service that you want part of the tracing, like a gRPC server for example:
+
+```go
+package example
+
+import (
+    "google.golang.org/grpc"
+	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+)
+
+type Server struct {
+	port       int
+	grpcServer *grpc.Server
+}
+
+func NewServer(
+	port int,
+	patientGatewayServiceV1 *PatientGatewayServiceV1,
+) *Server {
+
+	grpcServer := grpc.NewServer(
+		grpc.UnaryInterceptor(otelgrpc.UnaryServerInterceptor()),   // instrumentation
+		grpc.StreamInterceptor(otelgrpc.StreamServerInterceptor()), // instrumentation
+	)
+
+	patientGatewayServiceV1.Register(grpcServer)
+
+	return &Server{
+		port:       port,
+		grpcServer: grpcServer,
+	}
+}
+```
---
+++ pkg/telemetry/tracing.go
@@ -0,0 +1,108 @@
+package telemetry
+
+import (
+	"context"
+	"time"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
+)
+
+var defaultOtlpCollectorHttpEndpoint = "opentelemetry-collector:80"
+var defaultOtlpCollectorTimeoutSecs = 30
+var defaultStdoutExporterEnabled = false
+
+type TracingConfig struct {
+	ServiceName           string
+	ServiceVersion        string
+	DeploymentEnvironment string
+
+	OtlpExporterEnabled       bool
+	OtlpCollectorHttpEndpoint *string
+	OtlpCollectorTimeoutSecs  *int
+
+	StdoutExporterEnabled *bool
+}
+
+func (c *TracingConfig) setDefaults() {
+	if c.OtlpCollectorHttpEndpoint == nil {
+		c.OtlpCollectorHttpEndpoint = &defaultOtlpCollectorHttpEndpoint
+	}
+	if c.OtlpCollectorTimeoutSecs == nil {
+		c.OtlpCollectorTimeoutSecs = &defaultOtlpCollectorTimeoutSecs
+	}
+	if c.StdoutExporterEnabled == nil {
+		c.StdoutExporterEnabled = &defaultStdoutExporterEnabled
+	}
+}
+
+func SetupTracing(
+	ctx context.Context,
+	config *TracingConfig,
+) func() {
+	config.setDefaults()
+
+	// Create a resource describing this application
+	res, err := resource.New(
+		ctx,
+		resource.WithAttributes(
+			semconv.ServiceNameKey.String(config.ServiceName),
+			semconv.ServiceVersionKey.String(config.ServiceVersion),
+			semconv.DeploymentEnvironmentKey.String(config.DeploymentEnvironment),
+		),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// Read this for an explanation to why we always sample:
+	// https://anecdotes.dev/opentelemetry-on-google-cloud-unraveling-the-mystery-f61f044c18be
+	sampler := sdktrace.AlwaysSample()
+
+	var traceExporter sdktrace.SpanExporter
+	if config.OtlpExporterEnabled {
+		// Setup an otlp trace exporter that will send traces to a collector
+		otlptraceClient := otlptracehttp.NewClient(
+			otlptracehttp.WithEndpoint(*config.OtlpCollectorHttpEndpoint),
+			otlptracehttp.WithInsecure(),
+			otlptracehttp.WithTimeout(time.Duration(*config.OtlpCollectorTimeoutSecs)*time.Second),
+		)
+		traceExporter, err = otlptrace.New(ctx, otlptraceClient)
+	} else if *config.StdoutExporterEnabled {
+		// Setup a stdout trace exporter that just pretty prints
+		traceExporter, err = stdouttrace.New(stdouttrace.WithPrettyPrint())
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	// Register the trace exporter with a TracerProvider, using
+	// a batch span processor to aggregate spans before export
+	bsp := sdktrace.NewBatchSpanProcessor(traceExporter)
+
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithResource(res),
+		sdktrace.WithSampler(sampler),
+		sdktrace.WithSpanProcessor(bsp),
+	)
+	otel.SetTracerProvider(tracerProvider)
+
+	// Register the trace context and baggage propagators so data is propagated across services/processes
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	))
+
+	return func() {
+		tracerProvider.ForceFlush(ctx)
+		tracerProvider.Shutdown(ctx)
+	}
+}
